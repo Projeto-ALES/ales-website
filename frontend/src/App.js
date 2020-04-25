80873c3087a45616dd97da775f586cfe63583b7e
vpalmerini <victorpalmerini@gmail.com>
2020-04-25 12:42:43 -0300

    feat: create sidebar skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import SideBar from "components/SideBar/SideBar";
+
 import Home from "views/Home/Home";
 
 function App() {
   return (
     <div>
+      <SideBar />
       <Home />
     </div>
   );
---
+++ frontend/src/components/SideBar/SideBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./SideBar.module.scss";
+
+const SideBar = () => {
+  return <nav className={styles.sideBarContainer}></nav>;
+};
+
+export default SideBar;
