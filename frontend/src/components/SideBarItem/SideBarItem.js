5fef9eba4759f27fb3d0573751dceecdd887672f
vpalmerini <victorpalmerini@gmail.com>
2020-05-01 12:04:36 -0300

    ref: align text of sidebar items

--- frontend/src/components/SideBarItem/SideBarItem.js
+++ frontend/src/components/SideBarItem/SideBarItem.js
@@ -5,11 +5,15 @@ import styles from "./SideBarItem.module.scss";
 const SideBarItem = ({ text, icon, isOpen }) => {
   return (
     <li className={styles.itemContainer}>
-      <a className={`${styles.itemLink} ${isOpen && styles.isOpenLink}`} href="#">
-        <span style={{ color: "white", fontSize: "1.5em" }}>
-          <i class={icon}></i>
-        </span>
-        <span className={`${styles.itemText} ${isOpen && styles.isOpenText}`}>{text}</span>
+      <a className={`${styles.itemLink}`} href="#">
+        <div>
+          <span style={{ color: "white", fontSize: "1.5em" }}>
+            <i class={icon}></i>
+          </span>
+        </div>
+        <div className={`${styles.itemText} ${isOpen && styles.isOpenText}`}>
+          <span>{text}</span>
+        </div>
       </a>
     </li>
   );
