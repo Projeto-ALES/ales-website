b51d2ab76b565b0d34adc4b034d53cf65b006efb
vpalmerini <victorpalmerini@gmail.com>
2020-05-09 18:21:30 -0300

    feat: add About section to Home

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 import Landing from "./Sections/Landing/Landing";
 import WhatIs from "./Sections/WhatIs/WhatIs";
 import WhatDo from "./Sections/WhatDo/WhatDo";
+import About from "./Sections/About/About";
 
 import styles from "./Home.module.scss";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <WhatIs />
       <div className={styles.line}></div>
       <WhatDo />
+      <About />
     </div>
   );
 };
