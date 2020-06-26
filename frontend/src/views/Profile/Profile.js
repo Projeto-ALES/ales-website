ca8b2fdebf0844eb5a94151eaab28fa01590a0b4
vpalmerini <victorpalmerini@gmail.com>
2020-06-25 23:23:21 -0300

    feat: add forms to Profile view

--- frontend/src/views/Profile/Profile.js
+++ frontend/src/views/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Container from "components/Container/Container";
+import Input from "components/Input/Input";
 
 import styles from "./Profile.module.scss";
 
@@ -10,6 +11,28 @@ const Profile = () => {
       <Container>
         <div className={styles.profileTitle}>
           <h2>Meu Perfil</h2>
+        </div>
+        <div className={styles.formsContainer}>
+          <form className={styles.forms}>
+            <div className={styles.formsSection}>
+              <span>Dados Principais</span>
+              <Input label="Nome" type="text" />
+              <Input label="Email" type="text" />
+              <Input label="Telefone" type="text" />
+            </div>
+            <div className={styles.formsSection}>
+              <span>Dados Opcionais</span>
+              <Input label="Data de Nascimento" type="text" />
+              <Input label="Gênero" type="text" />
+              <Input label="Curso/Área de Trabalho" type="text" />
+            </div>
+            <div className={styles.formsSection}>
+              <span>Autenticação</span>
+              <Input label="Senha Atual" type="password" />
+              <Input label="Nova Senha" type="password" />
+              <Input label="Confirmação da Nova Senha" type="password" />
+            </div>
+          </form>
         </div>
       </Container>
     </div>
