559250fe3f2264283d6d0196108995dc7ea86a59
vpalmerini <victorpalmerini@gmail.com>
2020-07-06 10:34:40 -0300

    ref: refactor user.service with promises instead of try/catch blocks

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,21 +1,21 @@
 const User = require("../models/user");
 
 exports.getUsers = async query => {
-  try {
-    let users = await User.find(query);
-    return users;
-  } catch (e) {
-    throw Error("Error while Paginating Users");
-  }
+  return await User.find(query)
+    .then(users => {
+      return users;
+    })
+    .catch(err => {
+      throw Error(err.message);
+    });
 };
 
 exports.createUser = async data => {
-  try {
-    let user = await User.create(data).then(() => {
+  return await User.create(data)
+    .then(() => {
       return User.find({ email: data.email });
+    })
+    .catch(err => {
+      throw Error(err.message);
     });
-    return user;
-  } catch (e) {
-    throw Error("Error while creating user");
-  }
 };
