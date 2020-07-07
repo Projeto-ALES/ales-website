6c8fbf208da76f1dc8d72cc4ae38aeb1a54a6321
vpalmerini <victorpalmerini@gmail.com>
2020-07-07 16:21:38 -0300

    feat: integrate login (success flow only)

---
+++ frontend/src/services/auth.service.js
@@ -0,0 +1,8 @@
+import api from "./api";
+
+export const login = async (email, password) => {
+  return await api.post("/login", {
+    email,
+    password,
+  });
+};
--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { login } from "services/auth.service";
 
 import Container from "components/Container/Container";
 import Card from "components/Card/Card";
@@ -10,20 +12,50 @@ import Alessauro from "assets/logos/alessauro.svg";
 import styles from "./Login.module.scss";
 
 const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitLogin = async (e, email, password) => {
+    e.preventDefault();
+
+    login(email, password)
+      .then(() => {
+        history.push("/my-area");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className={styles.loginContainer}>
       <Container>
         <div className={styles.cardContainer}>
           <Card kind="outline-blue">
-            <form className={styles.cardContentContainer}>
+            <form
+              className={styles.cardContentContainer}
+              onSubmit={(e) => submitLogin(e, email, password)}
+            >
               <img src={Alessauro} alt="alessauro" />
               <div className={styles.inputsContainer}>
-                <Input label="Email" type="text" />
-                <Input label="Senha" type="password" />
+                <Input
+                  placeholder="Email"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <Input
+                  placeholder="Senha"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
                 <a href="/reset-password">Esqueci a senha</a>
               </div>
               <div className={styles.buttonContainer}>
-                <Button text="Entrar" kind="primary" onClick={() => history.push("/my-area")} />
+                <Button text="Entrar" kind="primary" type="submit" />
               </div>
             </form>
           </Card>
