0aafbb23c0986a7c9ef2510b6c2c85fc28b6afea
vpalmerini <victorpalmerini@gmail.com>
2020-07-08 22:48:58 -0300

    feat: add context to App with isLoggedIn state

---
+++ frontend/src/store/store.js
@@ -0,0 +1,16 @@
+import React, { createContext, useReducer } from "react";
+import { reducer } from "./reducer";
+
+const initialState = {
+  isLoggedIn: false,
+};
+
+const context = createContext(initialState);
+const { Provider } = context;
+
+const Store = ({ children }) => {
+  const state = useReducer(reducer, initialState);
+  return <Provider value={state}>{children}</Provider>;
+};
+
+export { context, Store };
