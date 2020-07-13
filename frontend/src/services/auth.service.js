d128ae1f5e53250d7aa6e16873a9ef01db24f86d
vpalmerini <victorpalmerini@gmail.com>
2020-07-12 22:34:19 -0300

    feat: integrate new password (by token) functionality

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -20,3 +20,11 @@ export const logout = async () => {
 export const resetPassword = async (email) => {
   return await api.post("/reset-password", { email }, { withCredentials: true });
 };
+
+export const newPassword = async (new_password, new_password_conf, token) => {
+  return await api.post(
+    "/new-password",
+    { new_password, new_password_conf, token },
+    { withCredentials: true }
+  );
+};
--- frontend/src/views/NewPassword/NewPassword.js
+++ frontend/src/views/NewPassword/NewPassword.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { newPassword } from "services/auth.service";
+import { toast } from "react-toastify";
 
 import Container from "components/Container/Container";
 import Card from "components/Card/Card";
 import Input from "components/Input/Input";
 import Button from "components/Button/Button";
 
-import { useHistory } from "react-router-dom";
-
 import styles from "./NewPassword.module.scss";
 
-const NewPassword = () => {
-  let history = useHistory();
+const NewPassword = ({ history, match }) => {
+  const [password, setPassword] = useState("");
+  const [passwordConf, setPasswordConf] = useState("");
+
+  const submitNewPassword = (e, new_password, new_password_conf, token) => {
+    e.preventDefault();
+
+    if (new_password !== new_password_conf) {
+      toast.warning("A senha e sua confirmação devem ser iguais");
+      return;
+    }
+
+    newPassword(new_password, new_password_conf, token)
+      .then(() => {
+        history.push("/password-changed");
+        toast.success("Senha alterada com sucesso");
+      })
+      .catch((err) => {
+        toast.error(
+          "Ops! Parece que a requisição pra cadastrar uma nova senha expirou :( Tente gerar outra requisição"
+        );
+      });
+  };
 
   return (
     <div className={styles.newPasswordContainer}>
       <Container>
         <div className={styles.cardContainer}>
           <Card kind="outline-blue">
-            <div className={styles.cardContentContainer}>
+            <form
+              className={styles.cardContentContainer}
+              onSubmit={(e) => submitNewPassword(e, password, passwordConf, match.params.token)}
+            >
               <div className={styles.cardTitle}>
                 <h3>Nova Senha</h3>
               </div>
@@ -25,15 +50,23 @@ const NewPassword = () => {
                 Defina uma nova senha para poder entrar no nosso site :)
               </div>
               <div className={styles.inputsContainer}>
-                <Input placeholder="Nova Senha" type="password" required />
-                <Input placeholder="Nova Senha" type="password" required />
+                <Input
+                  placeholder="Nova Senha"
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                  required
+                />
+                <Input
+                  placeholder="Confirmação da Nova Senha"
+                  type="password"
+                  onChange={(e) => setPasswordConf(e.target.value)}
+                  value={passwordConf}
+                  required
+                />
               </div>
-              <Button
-                text="Redefinir Senha"
-                kind="success"
-                onClick={() => history.push("/password-changed")}
-              />
-            </div>
+              <Button text="Redefinir Senha" kind="success" type="submit" />
+            </form>
           </Card>
         </div>
       </Container>
