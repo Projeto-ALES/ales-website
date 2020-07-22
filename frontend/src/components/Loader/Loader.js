a31dab87d28bc7b45d263f592615cbdc75234ddd
vpalmerini <victorpalmerini@gmail.com>
2020-07-22 18:43:03 -0300

    feat: add Loader component

---
+++ frontend/src/components/Loader/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Loader.module.scss";
+
+const Loader = () => {
+  return <div className={styles.loaderContainer} />;
+};
+
+export default Loader;
