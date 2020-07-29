40d04b73619034a794889ccff92a413aaa0c972f
vpalmerini <victorpalmerini@gmail.com>
2020-07-28 21:11:50 -0300

    style: add width property to Button component

--- frontend/src/components/Button/Button.js
+++ frontend/src/components/Button/Button.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import styles from "./Button.module.scss";
 
-const Button = ({ type, text, onClick, kind, isLoading, disabled }) => {
+const Button = ({ type, text, onClick, kind, isLoading, disabled, width }) => {
   return (
     <button
       type={type}
       className={`${styles.button} ${styles[kind]}`}
       onClick={onClick}
       disabled={disabled}
+      style={{ width }}
     >
       {isLoading ? <div className={styles.loader} /> : text}
     </button>
