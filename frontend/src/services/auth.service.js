b4d26511d64e5b8bdb20f83360da86b3ce35160d
vpalmerini <victorpalmerini@gmail.com>
2020-07-29 15:35:08 -0300

    ref: refactor withCredentials property of axios

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -3,14 +3,10 @@ import history from "routes/history";
 import routes from "routes/routes";
 
 export const login = async (email, password) => {
-  return await api.post(
-    "/login",
-    {
-      email,
-      password,
-    },
-    { withCredentials: true }
-  );
+  return await api.post("/login", {
+    email,
+    password,
+  });
 };
 
 export const logout = (error = false) => {
@@ -18,21 +14,13 @@ export const logout = (error = false) => {
 };
 
 export const resetPassword = async (email) => {
-  return await api.post("/reset-password", { email }, { withCredentials: true });
+  return await api.post("/reset-password", { email });
 };
 
 export const newPassword = async (new_password, new_password_conf, token) => {
-  return await api.post(
-    "/new-password",
-    { new_password, new_password_conf, token },
-    { withCredentials: true }
-  );
+  return await api.post("/new-password", { new_password, new_password_conf, token });
 };
 
 export const updatePassword = async (id, password, new_password, new_password_conf) => {
-  return await api.post(
-    `/update-password/${id}`,
-    { password, new_password, new_password_conf },
-    { withCredentials: true }
-  );
+  return await api.post(`/update-password/${id}`, { password, new_password, new_password_conf });
 };
--- frontend/src/services/professor.service.js
+++ frontend/src/services/professor.service.js
@@ -1,21 +1,21 @@
 import api from "./api";
 
 export const invite = async (email) => {
-  return await api.post("/invite-professor", { email }, { withCredentials: true });
+  return await api.post("/invite-professor", { email });
 };
 
 export const enroll = async (data) => {
-  return await api.post("/professors", data, { withCredentials: true });
+  return await api.post("/professors", data);
 };
 
 export const list = async () => {
-  return await api.get("/professors", { withCredentials: true });
+  return await api.get("/professors");
 };
 
 export const get = async (id) => {
-  return await api.get(`/professors/${id}`, { withCredentials: true });
+  return await api.get(`/professors/${id}`);
 };
 
 export const update = async (id, data) => {
-  return await api.put(`/professors/${id}`, data, { withCredentials: true });
+  return await api.put(`/professors/${id}`, data);
 };
