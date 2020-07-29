b5fde07592e0d059dce2415e5788c90e4039ebea
vpalmerini <victorpalmerini@gmail.com>
2020-07-29 12:49:23 -0300

    ref: add function to parse cookies and extract token and refresh token

---
+++ backend/src/helpers/cookie.js
@@ -0,0 +1,11 @@
+exports.parseCookie = async cookie => {
+  const rawCookies = cookie.split(";");
+  const parsedCookies = {};
+
+  await rawCookies.forEach(rawCookie => {
+    const parsedCookie = rawCookie.split("=");
+    parsedCookies[parsedCookie[0].trim()] = parsedCookie[1];
+  });
+
+  return parsedCookies;
+};
