593f375adfe3b01339fc03b97bd1dcef304e55d4
vpalmerini <victorpalmerini@gmail.com>
2020-07-30 17:10:14 -0300

    feat: integrate message form in Contact view

--- frontend/src/views/Contact/Contact.js
+++ frontend/src/views/Contact/Contact.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { send } from "services/mail.service";
 
 import Card from "components/Card/Card";
 import Input from "components/Input/Input";
 import TextArea from "components/TextArea/TextArea";
 import Button from "components/Button/Button";
+import { toast } from "react-toastify";
 
 import styles from "./Contact.module.scss";
 import items from "./items";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submitMessage = (e, data) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    send(data)
+      .then(() => {
+        toast.success("Mensagem enviada!");
+      })
+      .catch(() => {
+        toast.success("Ops! Ocorreu algum erro para enviar a mensagem");
+      })
+      .finally(() => {
+        setName("");
+        setEmail("");
+        setMessage("");
+        setIsSubmitting(false);
+      });
+  };
+
   return (
     <div className={styles.contactContainer}>
       <div className={styles.contactHeader}>
@@ -30,13 +58,41 @@ const Contact = () => {
       </div>
       <div className={styles.formContainer}>
         <h3>Mande uma mensagem!</h3>
-        <form className={styles.contactForm}>
+        <form
+          className={styles.contactForm}
+          onSubmit={(e) => submitMessage(e, { name, email, message })}
+        >
           <div className={styles.formContent}>
-            <Input placeholder="Nome" required />
-            <Input placeholder="Email" required />
-            <TextArea placeholder="Mensagem" rows={4} required />
+            <Input
+              placeholder="Nome"
+              value={name}
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            <Input
+              placeholder="Email"
+              value={email}
+              type="text"
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <TextArea
+              placeholder="Mensagem"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              rows={4}
+              required
+            />
           </div>
-          <Button text="Enviar" width="200px" />
+          <Button
+            text="Enviar"
+            type="submit"
+            width="200px"
+            kind="primary"
+            isLoading={isSubmitting}
+            disabled={isSubmitting}
+          />
         </form>
       </div>
     </div>
