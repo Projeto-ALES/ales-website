c99d196c8afe8a774ac2e481cd14f1d7e9e8388d
vpalmerini <victorpalmerini@gmail.com>
2020-08-01 15:03:05 -0300

    feat: add section to talk about adoption to pandemic scenario

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -3,6 +3,7 @@ import React from "react";
 import Landing from "./Sections/Landing/Landing";
 import WhatIs from "./Sections/WhatIs/WhatIs";
 import WhatDo from "./Sections/WhatDo/WhatDo";
+import Adaption from "./Sections/Adaption/Adaption";
 import About from "./Sections/About/About";
 
 import styles from "./Home.module.scss";
@@ -14,6 +15,7 @@ const Home = () => {
       <WhatIs />
       <div className={styles.line}></div>
       <WhatDo />
+      <Adaption />
       <About />
     </div>
   );
