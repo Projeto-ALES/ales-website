718c532edf65567ff358c4922c65602cc8e7bad0
vpalmerini <victorpalmerini@gmail.com>
2020-08-03 14:52:18 -0300

    ref: add configuration to connect to mongo in production environment

--- backend/src/mongo.js
+++ backend/src/mongo.js
@@ -1,19 +1,31 @@
 const mongoose = require("mongoose");
+const fs = require("fs");
 
 const {
   MONGO_INITDB_ROOT_USERNAME,
   MONGO_INITDB_ROOT_PASSWORD,
   MONGO_DATABASE,
   MONGO_HOSTNAME,
+  NODE_ENV,
 } = process.env;
 
-// TODO: migrate db url to configuration file
-const mongoAuth = `${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}`;
+let mongoURL;
+let DB_PASSWORD;
 
-const mongoUrl = `mongodb://${mongoAuth}@${MONGO_HOSTNAME}/${MONGO_DATABASE}?authSource=admin`;
+if (NODE_ENV === "production") {
+  DB_PASSWORD = fs.readFileSync(MONGO_INITDB_ROOT_PASSWORD, "utf-8");
+} else {
+  DB_PASSWORD = MONGO_INITDB_ROOT_PASSWORD;
+}
+
+const mongoAuth = `${MONGO_INITDB_ROOT_USERNAME}:${DB_PASSWORD.trim()}`;
+mongoURL = `mongodb://${mongoAuth}@${MONGO_HOSTNAME}/${MONGO_DATABASE}`;
 
 const connectDB = () => {
-  return mongoose.connect(mongoUrl, { useNewUrlParser: true });
+  return mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    auth: { authSource: "admin" },
+  });
 };
 
 module.exports = connectDB;
