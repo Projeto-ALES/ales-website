9aa43a03c2de41337578d0cb0587d90d7ffff579
vpalmerini <victorpalmerini@gmail.com>
2020-08-15 21:18:56 -0300

    feat: add PageTitle component

---
+++ frontend/src/components/PageTitle/PageTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './PageTitle.module.scss';
+
+const PageTitle = ({ title, icon }) => {
+    return (
+        <div className={styles.title}>
+            <h2 className={styles.title__text}>{title}</h2>
+            {icon && <span className={styles.title__icon}><i class={icon}></i></span>}
+        </div>
+    )
+}
+
+export default PageTitle;
