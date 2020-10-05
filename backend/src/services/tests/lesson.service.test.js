57477dca5d36f80efb9945df2a95181bbc3f5c3a
vpalmerini <victorpalmerini@gmail.com>
2020-10-04 21:32:05 -0300

    test: add tests for lesson services

---
+++ backend/src/services/tests/lesson.service.test.js
@@ -0,0 +1,69 @@
+const {
+  getLessonsBySubjectId,
+  getLessonById,
+  createLesson,
+  deleteLesson,
+  updateLesson,
+} = require("../lesson.service");
+
+const Lesson = require("../../models/lesson");
+jest.mock("../../models/lesson");
+
+describe("Lesson service", () => {
+  Lesson.find = jest.fn();
+  Lesson.findById = jest.fn();
+  Lesson.create = jest.fn();
+  Lesson.findByIdAndDelete = jest.fn();
+  Lesson.findOneAndUpdate = jest.fn();
+
+  const params = {
+    id: "lesson_id",
+    name: "lesson_name",
+    subject: {
+      id: "subject_id",
+    },
+  };
+
+  describe("getLessonsBySubjectId", () => {
+    it("should call Lesson.find with correct params", () => {
+      getLessonsBySubjectId(params.subject.id);
+      expect(Lesson.find).toHaveBeenCalledWith({ subject: params.subject.id });
+      expect(Lesson.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("getLessonById", () => {
+    it("should call Lesson.findById with correct params", () => {
+      getLessonById(params.id);
+      expect(Lesson.findById).toHaveBeenCalledWith(params.id);
+      expect(Lesson.findById).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("createLesson", () => {
+    it("should call Lesson.create with correct params", () => {
+      createLesson(params);
+      expect(Lesson.create).toHaveBeenCalledWith(params);
+      expect(Lesson.create).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("deleteLesson", () => {
+    it("should call Lesson.findByIdAndDelete with correct params", () => {
+      deleteLesson(params.id);
+      expect(Lesson.findByIdAndDelete).toHaveBeenCalledWith(params.id);
+      expect(Lesson.findByIdAndDelete).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("updateLesson", () => {
+    it("should call Lesson.findOneAndUpdate with correct params", () => {
+      updateLesson(params.id, params);
+      expect(Lesson.findOneAndUpdate).toHaveBeenCalledWith(
+        { _id: params.id },
+        { $set: { ...params } }
+      );
+      expect(Lesson.findOneAndUpdate).toHaveBeenCalledTimes(1);
+    });
+  });
+});
