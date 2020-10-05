0f0eaacf1f6343c23a8528170629d1e98c4b5d80
Hamitay <henrique.amitay@quintoandar.com.br>
2020-10-05 17:19:24 -0300

    Refactor Subject entity to Course entity

--- frontend/src/services/course.service.js
+++ frontend/src/services/course.service.js
@@ -1,21 +1,21 @@
 import api from "./api";
 
 export const list = async () => {
-  return await api.get("/subjects");
+  return await api.get("/courses");
 };
 
 export const create = async (data) => {
-  return await api.post("/subjects", data);
+  return await api.post("/courses", data);
 };
 
 export const get = async (id) => {
-  return await api.get(`/subjects/${id}`);
+  return await api.get(`/courses/${id}`);
 };
 
 export const update = async (id, data) => {
-  return await api.put(`/subjects/${id}`, data);
+  return await api.put(`/courses/${id}`, data);
 };
 
 export const remove = async (id) => {
-  return await api.delete(`/subjects/${id}`);
+  return await api.delete(`/courses/${id}`);
 };
