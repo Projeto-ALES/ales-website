c681398d8ad1b7718af361ef092535505f07ffaf
Victor Palmerini <victorpalmerini@gmail.com>
2020-11-08 19:16:25 -0300

    add CareerDay section and remove Subscription section from Home

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 import Landing from "./Sections/Landing/Landing";
 import WhatIs from "./Sections/WhatIs/WhatIs";
 import WhatDo from "./Sections/WhatDo/WhatDo";
-import Subscription from "./Sections/Subscription/Subscription";
+import CareerDay from "./Sections/CareerDay/CareerDay";
 import About from "./Sections/About/About";
 
 import styles from "./Home.module.scss";
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Landing />
-      <Subscription />
+      <CareerDay />
       <WhatIs />
       <div className={styles.line}></div>
       <WhatDo />
---
+++ frontend/src/views/Home/Sections/CareerDay/courses.js
@@ -0,0 +1,13 @@
+const schedule = [
+    { id: 1, time: "10h00 - 10h15", name: "Química" },
+    { id: 2, time: "10h15 - 10h30", name: "Ciência da Computação" },
+    { id: 3, time: "10h45 - 12h00", name: "Engenharias" },
+    { id: 4, time: "12h00 - 13h00", name: "ALMOÇO" },
+    { id: 5, time: "13h00 - 13h15", name: "Economia" },
+    { id: 6, time: "13h15 - 13h30", name: "Jornalismo" },
+    { id: 6, time: "13h30 - 13h45", name: "Midialogia" },
+    { id: 7, time: "14h00 - 14h15", name: "Farmácia" },
+    { id: 8, time: "14h15 - 14h30", name: "Medicina" },
+]
+
+export default schedule;
