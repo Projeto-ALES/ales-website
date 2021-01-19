1ac0e53b8cbebd449d392696b3e2224d0155bda2
Victor Palmerini <victorpalmerini@gmail.com>
2021-01-19 14:40:53 -0300

    ref: centralize mail controller in index file

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -6,5 +6,6 @@ router.use("/auth", require("./auth.controller"));
 router.use("/users", require("./user.controller"));
 router.use("/professors", require("./professor.controller"));
 router.use("/password", require("./password.controller"));
+router.use("/mail", require("./mail.controller"));
 
 module.exports = router;
