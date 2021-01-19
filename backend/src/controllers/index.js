1aa5b21e0ebf5fb58740d888ddea17edda2f8663
Victor Palmerini <victorpalmerini@gmail.com>
2021-01-19 14:39:30 -0300

    ref: centralize password controllers in index file

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.use("/auth", require("./auth.controller"));
 router.use("/users", require("./user.controller"));
 router.use("/professors", require("./professor.controller"));
+router.use("/password", require("./password.controller"));
 
 module.exports = router;
