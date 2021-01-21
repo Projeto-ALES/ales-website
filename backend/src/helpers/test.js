2cdd5cf21a2108ba7c900f4b654ec9eb94e92c8e
Victor Palmerini <victorpalmerini@gmail.com>
2021-01-21 17:04:48 -0300

    feat: add helper function to be used on unit tests

---
+++ backend/src/helpers/test.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const request = require("supertest");
+
+const User = require("../models/user");
+
+const createServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use("/api", require("../controllers/index"));
+  return app;
+};
+
+const createAgent = app => {
+  return request.agent(app);
+};
+
+const login = async (user, done) => {
+  const admin = { email: "admin@mail", password: "admin" };
+  await User.create(admin);
+
+  user
+    .post("/api/auth/login")
+    .send(admin)
+    .end((err, resp) => {
+      if (err) {
+        throw err;
+      }
+      done();
+    });
+};
+
+
+module.exports = {
+  createServer,
+  createAgent,
+  login,
+};
