4f765f7d3e86d980ec1859094e33602f4312c318
Victor Palmerini <victorpalmerini@gmail.com>
2021-02-08 12:23:25 -0300

    fix: set an end to the interval of google calendar events

--- backend/src/calendar/services/listEvents.js
+++ backend/src/calendar/services/listEvents.js
@@ -1,9 +1,10 @@
 const { calendar } = require("../calendar");
 
-const listEvents = async (id, start) => {
+const listEvents = async (id, start, end) => {
   const res = await calendar.events.list({
     calendarId: id,
     timeMin: start,
+    timeMax: end,
     singleEvents: true,
     orderBy: "startTime",
   });
