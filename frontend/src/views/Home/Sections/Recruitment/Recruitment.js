b4f247bea5943a9e5209b9407fed7a02e254b49d
jonaskw <66474846+jonaskw@users.noreply.github.com>
2021-02-10 18:16:58 -0300

    Update Recruitment.js

--- frontend/src/views/Home/Sections/Recruitment/Recruitment.js
+++ frontend/src/views/Home/Sections/Recruitment/Recruitment.js
@@ -12,7 +12,7 @@ const Recruitment = () => {
       <Container>
         <div className={styles.title}>
           <img src={Alessauro} alt="alessauro" />
-          <h2>Novas vagas para alunos !!</h2>
+          <h2>Novas vagas para alunos!!</h2>
         </div>
         <div className={styles.subtitle}>
           <h3>O Projeto ALES está com vagas abertas para alunos do 8º ano ao 3º ano do ensino médio.</h3>
@@ -22,39 +22,26 @@ const Recruitment = () => {
             O ano de 2021 já começou e o ALES já está se preparando para o retorno das aulas aos sábados. Neste semestre, seguiremos modelo online semelhante ao implementado no semestre passado, com o auxílio de ferramentas como Google Meet e Google Classroom. As aulas serão aos sábados, com as atividades iniciando às 9h30 e terminando às 17h30.
           </p>
           <p>
-            Temos vagas para as seguinte matérias neste semestre: <b>Inglês</b>, <b>Humanidades</b>, <b>Programação</b>, <b>Ciências</b>, <b>Matemática</b> e <b>Desenho</b>.
-          </p>
+            Temos vagas para professores: <b>Inglês</b>, <b>Humanidades</b>, <b>Programação</b>, <b>Ciências</b>, <b>Matemática</b> e <b>Desenho</b>.<br></br>
+              E pra área administrativa: <b>Comunicação</b>, <b>Pessoas</b>, <b>Tech</b>, <b>Financeiro</b> e <b>Sustentabilidade & Compliance</b></p>
           <p>
-            O nosso cronograma de aulas de sábado seguirá a seguinte agenda semanal:
-  <ul>9h30 - 10h30: Inglês</ul>
-  <ul>10h35 - 11h50: Humanidades</ul>
-  <ul>11h50 - 13h00: Período de Almoço</ul>
-  <ul>13h00 - 14h00: Programação</ul>
-  <ul>14h05 - 15h20: Ciências</ul>
-  <ul>15h25 - 16h25: Matemática</ul>
-  <ul>16h30 - 17h30: Desenho</ul>
-  Você pode escolher quantas matérias quiser (pode fazer uma só ou todas!!!)
-          </p>
-  <p>
-  As inscrições vão de <b>10/02</b> até <b>19/02</b> e podem ser feitas pelo formulário a seguir:
+            As inscrições vão de <b>25/01</b> até <b>29/01</b> e podem ser feitas no formulário a seguir:
             </p>
           <div className={styles.buttons}>
             <a
               className={styles.button}
-              href="https://docs.google.com/forms/d/e/1FAIpQLSeba10ZJF0aJj3382-g2eUZINBuDPTDCaO8BFTRih-eWOnvBQ/viewform?usp=pp_url"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSc2C2-mkRGdAAeVYTfSxgochVZdAfcEu0VJekqWmyqWyRAwAA/viewform"
               target="_blank"
               rel="noopener noreferrer"
             >
               <ButtonIcon text="Inscrição" kind="blue" icon="far fa-file-alt" />
             </a>
           </div>
-          </p>
-          </div>
+        </div>
         <div className={styles.info}>
           <p>Todo o processo ocorrerá de forma remota, assim como as atividades realizadas ao longo do semestre. <br></br>
-              </p>
+          </p>
         </div>
- 
       </Container >
     </div >
   )
