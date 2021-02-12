b45265c21a74aa918e8821a938f48a79c847f4a2
Victor Palmerini <victorpalmerini@gmail.com>
2021-02-12 17:04:03 -0300

    ref: standardize dates format in separateByDays function

--- backend/src/calendar/helpers/separateByDays.js
+++ backend/src/calendar/helpers/separateByDays.js
@@ -4,10 +4,14 @@ const separateByDays = async (events) => {
   await events.forEach(e => {
     let date;
     if (e.start.dateTime) {
-      date = new Date(e.start.dateTime);
-      date = date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+      date = new Date(e.start.dateTime).toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
     } else if (e.start.date) {
-      date = e.start.date;
+      date = new Date(e.start.date);
+      date = ("0" + date.getUTCDate()).slice(-2) + "/" + ("0" + (date.getUTCMonth() + 1)).slice(-2) + "/" + date.getUTCFullYear();
     }
     if (items[date] !== undefined) {
       items[date].push(e);
