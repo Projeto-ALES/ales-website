34448f67d805c267210a5f7461d2ab9ec2bb71d2
T. H. de Camargo J <thdecamargoj@gmail.com>
2021-03-25 21:21:18 -0300

    Modificações na reunião 25/03/2021

--- frontend/src/components/SideBar/items.js
+++ frontend/src/components/SideBar/items.js
@@ -1,9 +1,11 @@
 import { logout } from "services/auth.service";
 
 const notLoggedIn = [
-  { id: 1, text: "Home", icon: "fas fa-home", to: "/", onClick: () => {} },
-  { id: 2, text: "Contato", icon: "fas fa-paper-plane", to: "/contact", onClick: () => {} },
-  { id: 3, text: "Entrar", icon: "fa fa-sign-in", to: "/login", onClick: () => {} },
+  { id: 1, text: "Home", icon: "fas fa-home", to: "/", onClick: () => { } },
+  { id: 2, text: "Contato", icon: "fas fa-paper-plane", to: "/contact", onClick: () => { } },
+  { id: 3, text: "Entrar", icon: "fa fa-sign-in", to: "/login", onClick: () => { } },
+  { id: 4, text: "Seja Aluno", icon: "fas fa-user-graduate", to: "/aluno", onClick: () => { } },
+  { id: 5, text: "Seja Voluntário", icon: "fas fa-hands-helping", to: "/volunteer", onClick: () => { } },
 ];
 
 const loggedIn = [
@@ -13,7 +15,7 @@ const loggedIn = [
     text: "Matérias",
     icon: "far fa-calendar-check",
     to: "/quarentenales",
-    onClick: () => {},
+    onClick: () => { },
   },
   {
     id: 3,
