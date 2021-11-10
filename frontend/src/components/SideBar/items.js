61ed8a706deac5110fe35a90c1a5e577f4eaa083
Sofia Moreira <scmsofia@gmail.com>
2021-11-09 22:24:11 -0300

    modificações reunião 09nov

--- frontend/src/components/SideBar/items.js
+++ frontend/src/components/SideBar/items.js
@@ -1,11 +1,27 @@
 import { logout } from "services/auth.service";
 
 const notLoggedIn = [
-  { id: 1, text: "Home", icon: "fas fa-home", to: "/", onClick: () => { } },
-  // { id: 2, text: "Contato", icon: "fas fa-paper-plane", to: "/contact", onClick: () => { } },
+  { id: 1, text: "Home", icon: "fas fa-home", to: "/", onClick: () => { window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });} },
+   { id: 2, text: "Contato", icon: "fas fa-paper-plane", to: "/contact", onClick: () => { window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });} },
   // { id: 3, text: "Entrar", icon: "fa fa-sign-in", to: "/login", onClick: () => { } },
-  { id: 4, text: "Seja Aluno", icon: "fas fa-user-graduate", to: "/aluno", onClick: () => { } },
-  { id: 5, text: "Seja Voluntário", icon: "fas fa-hands-helping", to: "/volunteer", onClick: () => { } },
+  { id: 4, text: "Seja Aluno", icon: "fas fa-user-graduate", to: "/aluno", onClick: () => {window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });} },
+  { id: 5, text: "Seja Voluntário", icon: "fas fa-hands-helping", to: "/volunteer", onClick: () => {window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  }); } },
 ];
 
 const loggedIn = [
