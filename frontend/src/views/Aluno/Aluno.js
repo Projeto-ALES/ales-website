fa0f7020f6ebc1c8d509eaefeeee46161e84270f
Sofia Moreira <scmsofia@gmail.com>
2021-11-27 00:49:05 -0300

    mexendo_textos

--- frontend/src/views/Aluno/Aluno.js
+++ frontend/src/views/Aluno/Aluno.js
@@ -20,16 +20,16 @@ const Volunteer = () => {
             <div className={styles.container}>
                 <Container>
                     <div className={styles.title}>
-                        <h1 className="mt-5">Quer ser aluno ou aluna do projeto ALES?</h1>
+                        <h1 className="mt-5">Quer estudar no projeto ALES?</h1>
                     </div>
 
                     <div className={styles.description}>
                         <p>
-                            As aulas desse semestre já começaram, mas se você quiser participar do próximo semestre, basta preencher o nosso formulário de interesse (disponível no final desta página). Dessa forma, quando abrirmos as vagas para o próximo semestre, entraremos em contato com você e enviaremos todas as informações necessárias, assim como o formulário de inscrição.
+                        Deseja participar no próximo semestre? Preencha o formulário de interesse disponível no final desta página.
                         </p>
 
                         <p>
-                            <strong><big>Por que ser um aluno do ALES?</big></strong>
+                            <strong><big>Por que estudar no ALES?</big></strong>
                             <p></p>
                             <ul>
                                 <li>Aprender conteúdos diferentes dos oferecidos nas escolas;</li>
@@ -45,15 +45,15 @@ const Volunteer = () => {
                             <ul>
                                 <li><strong>CIÊNCIAS:</strong> Buscamos atiçar a curiosidade natural que todos nós temos para entender o funcionamento da natureza e para enxergar como a biologia, a física e a química estão presentes no nosso dia-a-dia.</li>
                                 <li><strong>DESENHO:</strong> Queremos que você possa colocar as imagens de sua cabeça no papel! Para tornar isso possível vamos estudar fundamentos importantes como desenho de observação, corpo humano, luz e sombra, entre muitos outros. Venha com a gente nessa jornada!</li>
-                                <li><strong>HUMANIDADES:</strong> Procuramos quebrar as barreiras entre filosofia, literatura, política, arte, mitologia e outras áreas do conhecimento humanístico para entender a História através de uma perspectiva distinta.</li>
+                                <li><strong>HUMANIDADES:</strong> Procuramos quebrar as barreiras entre filosofia, literatura, política, arte, mitologia e outras áreas do conhecimento humanístico para entender a História através de uma outra perspectiva.</li>
                                 <li><strong>INGLÊS:</strong> Trabalhamos todas as faces da aprendizagem de uma língua estrangeira para iniciantes, desde a fala e a escuta até a gramática e a escrita, incentivando a busca pelo conhecimento da língua inglesa em práticas prazerosas, como na música ou no cinema.</li>
-                                <li><strong>MATEMÁTICA:</strong> Buscamos ir além das contas e desenvolver o raciocínio lógico, a leitura e a interpretação de texto, mostrando como a matemática pode ser divertida e está presente em todo lugar no nosso dia-a-dia!</li>
+                                <li><strong>MATEMÁTICA:</strong> Buscamos ir além das contas e desenvolver o raciocínio lógico, a leitura e a interpretação de texto. Mostrando como a matemática pode ser divertida e está presente em todo lugar no nosso dia-a-dia!</li>
                                 <li><strong>PROGRAMAÇÃO:</strong> Nosso objetivo em programação é estimular os alunos a enfrentar novos desafios numa matéria que nem sempre é oferecida no ensino comum. Onde podemos desenvolver jogos e aplicações de uma forma divertida e prática, abordando desde os conceitos mais básicos da programação.</li>
                             </ul>
                         </p>
 
                         <p>
-                            Venha ser nosso aluno! Você pode se inscrever em quantas disciplinas quiser! Clique no link abaixo e preencha o formulário. Assim que as nossas inscrições abrirem, entraremos em contato com você/seu responsável!
+                        Venha desbravar diferentes aprendizados com a gente! Você pode se inscrever em quantas disciplinas quiser! Clique no link abaixo e preencha o formulário. Assim que as nossas inscrições abrirem, entraremos em contato com você/seu responsável!
                         </p>
 
                         <div className={styles.buttons}>
--- frontend/src/views/Home/Sections/WhatIs/WhatIs.js
+++ frontend/src/views/Home/Sections/WhatIs/WhatIs.js
@@ -15,29 +15,11 @@ const WhatIs = () => {
         </div>
         <div className={styles.whatisDescription}>
           <p>
-            Acreditamos que a curiosidade e a vontade de aprender são essenciais na vida dos jovens,
-            mas muitas vezes estes são suprimidos pela falta de acesso às informações corretas ou
-            são desestimulados pelo sistema de ensino que mantemos em nosso país.
+          A curiosidade e a vontade de aprender são essenciais na vida dos jovens, mas muitas vezes estas são suprimidas pela falta de acesso às informações e sistema de ensino vigente em nosso país. Reconhecemos que a culpa disso não é das escolas e desta percepção nasceu o ALES, um projeto que tem como objetivo trazer conteúdos além da escola, complementando o desenvolvimento escolar e pessoal de estudantes do ensino médio, em especial da rede pública.
+Nosso projeto não é um substituto da escola, e sim uma ferramenta para expandir horizontes e inspirar estudantes a irem além na busca por novos conhecimentos.
+
           </p>
-          <p>
-            Reconhecemos que a culpa disso não é das escolas. Muitas vezes faltam, em especial nas
-            públicas, as ferramentas e oportunidades para abordar temas extra-curriculares, por mais
-            importantes que eles sejam. Desta percepção nasceu o ALES, um projeto que tem como
-            objetivo trazer complementos acadêmicos e intelectuais, fora das escolas, de forma
-            gratuita aos alunos da comunidade de Barão Geraldo, Campinas.
-          </p>
-          <p>
-            Os alunos que tiverem interesse em participar do projeto terão a oportunidade de
-            crescer, se conhecer e aprender através do contato com conteúdos que dificilmente teriam
-            acesso apenas através da rede pública de ensino. Nosso projeto, portanto, não é um
-            substituto da escola. É uma ferramenta para expandir horizontes e inspirar alunos a
-            fazerem algo novo.
-          </p>
-          <p>
-            É a nossa vontade de mostrar para os alunos que conhecimento é muito mais do que as
-            coisas que aprendem na escola e que eles também podem aprender, alcançar e criar coisas
-            que antes eles pensavam, ou eram ditos, serem distantes demais de sua realidade.
-          </p>
+          
         </div>
       </div>
     </Container>
