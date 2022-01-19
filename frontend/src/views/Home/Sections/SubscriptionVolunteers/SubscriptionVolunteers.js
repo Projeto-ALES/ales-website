e9bdc1c312ab2fe79ac231c83c85a84a243df86e
jonaskw <66474846+jonaskw@users.noreply.github.com>
2022-01-19 12:11:33 -0300

    Update SubscriptionVolunteers.js

--- frontend/src/views/Home/Sections/SubscriptionVolunteers/SubscriptionVolunteers.js
+++ frontend/src/views/Home/Sections/SubscriptionVolunteers/SubscriptionVolunteers.js
@@ -9,32 +9,30 @@ const SubscriptionVolunteers = () => {
     <div className={styles.container} id="recrutamento">
       <Container>
         <div className={styles.title}>
-          <img src={Alessauro} alt="alessauro" />
           <h3><b>Inscrições abertas para voluntáries! Venha fazer a diferença na educação com a gente!</b></h3>
         </div>
         <div className={styles.subtitle}>
-          <h4>O Projeto ALES está com vagas abertas para qualquer pessoa que queira voluntariar-se. Somos majoritariamente</h4>
+          <h4>O Projeto ALES está com vagas abertas para qualquer pessoa que queira voluntariar-se.</h4>
         </div>
         <div className={styles.description}>
           <p>
-            O primeiro semestre letivo de 2022 está iniciando e o ALES já está se preparando para o retorno das aulas aos sábados. Neste semestre, seguiremos modelo online semelhante ao implementado no semestre passado, com o auxílio de ferramentas como Google Meet e Google Classroom. As aulas serão aos sábados, com as atividades iniciando às 9h30 e terminando às 17h30.
+            O primeiro semestre letivo de 2022 está iniciando e o ALES já está se preparando para o retorno das aulas. Neste semestre, seguiremos modelo online semelhante ao implementado no semestre passado, com o auxílio de ferramentas como Google Meet e Google Classroom.
         
           </p>
+          
           <p>
-            Temos vagas para as seguinte matérias neste semestre: <b>Inglês</b>, <b>Humanidades</b>, <b>Programação</b>, <b>Ciências</b>, <b>Matemática</b> e <b>Desenho</b>. </p>
-
-          <p>
-            O nosso cronograma de aulas de sábado seguirá a seguinte agenda semanal:
+            Possuímos vagas para as seguintes 
           </p>
           <div className={styles.table}>
             <table>
+            Administrativo
               <tr>
                 <td>Comunicação</td>
                 <td>Financeiro</td>
                 <td>Pessoas</td>
                 <td>TECH</td>
               </tr>
-
+  Ensino
               <tr>
                 <td>Desenho</td>
                 <td>Inglês</td>
@@ -46,21 +44,37 @@ const SubscriptionVolunteers = () => {
             </table>
           </div>
 
-          <p>Você pode escolher quantas matérias quiser (pode fazer uma só ou todas!!!)</p>
-          <p>
-            As inscrições vão de <b>02/08</b> até <b>13/08</b> e podem ser feitas pelo formulário a seguir:
+<p>
+            Você pode conferir nosso edital no link abaixo:
+            </p>
+
+<div className={styles.buttons}>
+            <a
+              className={styles.button}
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfCtFWbTMnEreqQ61mImHAmSoLH5euHQfrlWWCOtsYELt89Tw/viewform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ButtonIcon text="Edital" kind="secondary" icon="far fa-file-alt" />
+            </a>
+          </div>          
+
+<p>
+            As inscrições vão até <b>28/01</b> e podem ser feitas pelo formulário a seguir:
             </p>
 
           <div className={styles.buttons}>
             <a
               className={styles.button}
-              href="https://docs.google.com/forms/u/2/d/e/1FAIpQLSeQrmxQmucx6GUc8gvzQD8MNw1dsuidWDVXMs8gTZNpYtwQjA/viewform"
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfCtFWbTMnEreqQ61mImHAmSoLH5euHQfrlWWCOtsYELt89Tw/viewform"
               target="_blank"
               rel="noopener noreferrer"
             >
               <ButtonIcon text="Inscrição" kind="secondary" icon="far fa-file-alt" />
             </a>
           </div>
+
+
         </div>
         <div className={styles.info}>
           <p>Todo o processo ocorrerá de forma remota, assim como as atividades realizadas ao longo do semestre. <br></br>
