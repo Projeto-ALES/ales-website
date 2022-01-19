fae2189fb2f35ede09f7c496ea72db9b65a51fc4
jonaskw <66474846+jonaskw@users.noreply.github.com>
2022-01-19 15:32:33 -0300

    Update SubscriptionVolunteers.js

--- frontend/src/views/Home/Sections/SubscriptionVolunteers/SubscriptionVolunteers.js
+++ frontend/src/views/Home/Sections/SubscriptionVolunteers/SubscriptionVolunteers.js
@@ -25,7 +25,7 @@ const SubscriptionVolunteers = () => {
           </p>
           <div className={styles.table}>
             <table>
-            <p><b>Administrativo</b></p>
+            <p style="text-align:center"><b>Administrativo</b></p>
               <tr>
                 <td>Comunicação</td>
                 <td class = "branco"></td>
